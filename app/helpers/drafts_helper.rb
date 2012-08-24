30deac50d68d7f02033ba80d9f330198badfae25
weyewe <w.yunnal@gmail.com>
2012-08-24 21:42:54 +0700

    cleared posting

--- app/helpers/drafts_helper.rb
+++ app/helpers/drafts_helper.rb
@@ -4,4 +4,12 @@ module DraftsHelper
       return  'default-hidden'
     end
   end
+  
+  def get_special_date_calendar_for_draft(job_request, today_date )
+    if job_request.finish_date.nil? and today_date > job_request.ending_date 
+      today_date
+    else 
+      job_request.ending_date  
+    end 
+  end
 end
